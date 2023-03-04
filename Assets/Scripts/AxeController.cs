3aeb71891c903655366ceb5620fcc28d66995b3b
Guiller977 <guigaltnt@gmail.com>
2023-03-04 17:59:15 +0100

    Axes fixed
    it works now :)

--- Assets/Scripts/AxeController.cs
+++ Assets/Scripts/AxeController.cs
@@ -8,20 +8,24 @@ public class AxeController : MonoBehaviour
     private Vector3 target;
     private Vector2 position;
     public Rigidbody2D theRB;
+    public Transform playerpos;
+    public float diference;
 
     void Start()
     {
+        playerpos = Playercontroller.sharedInstance.transform;
         theRB = GetComponent<Rigidbody2D>();
-        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Update()
     {
         Throw();
+        Debug.Log(target);
     }
 
     void Throw()
     {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition) - new Vector3(playerpos.position.x, playerpos.position.y, playerpos.position.z);
         float step = speed * Time.deltaTime;
         theRB.AddForce(target, ForceMode2D.Impulse);
     }
